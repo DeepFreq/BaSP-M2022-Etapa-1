31a005fda9f2b550d3cd3b568f0c7255bae64570
Alan Acosta <alanegocios77@gmail.com>
2022-04-18 19:29:41 -0300

    BaSP-2022: ex01-Finish ex. Var&Operators

--- semana-05/excercises/01_variables_and_operators.js
+++ semana-05/excercises/01_variables_and_operators.js
@@ -1,4 +1,7 @@
+//VARIABLES & OPERATORS//
+console.log('(--Var&Operators--)');
 // (A) //
+console.log('(A)');
 
 var numberOne = 1;
 var numberSeven = 7;
@@ -6,15 +9,17 @@ var sumResult = numberOne + numberSeven;
 console.log(sumResult);
 
 // (B) //
+console.log('(B)');
 
-var myNameIs = "My name is";
-var myFirstName = "Alan";
+var myNameIs = 'My name is';
+var myFirstName = 'Alan';
 var iAm = myNameIs + " " + myFirstName;
 console.log(iAm);
 
 // (C) //
+console.log('(C)');
 
-var thisWords = "This is a sentence.";
-var otherWords = " This is a other sentence";
+var thisWords = 'This is a sentence.';
+var otherWords = '' This is a other sentence';
 var addTheWords = thisWords.length + otherWords.length;
 console.log(addTheWords);
