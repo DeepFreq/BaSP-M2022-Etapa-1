0e36da035d78f19b9d5a5394ad3c5e2b250037e1
Alan Acosta <alanegocios77@gmail.com>
2022-04-18 01:11:02 -0300

    BaSP-2022: ex01-Add-Files-AndFirstEx.

---
+++ semana-05/excercises/01_variables_and_operators.js
@@ -0,0 +1,20 @@
+// (A) //
+
+var numberOne = 1;
+var numberSeven = 7;
+var sumResult = numberOne + numberSeven;
+console.log(sumResult);
+
+// (B) //
+
+var myNameIs = "My name is";
+var myFirstName = "Alan";
+var iAm = myNameIs + " " + myFirstName;
+console.log(iAm);
+
+// (C) //
+
+var thisWords = "This is a sentence.";
+var otherWords = " This is a other sentence";
+var addTheWords = thisWords.length + otherWords.length;
+console.log(addTheWords);
