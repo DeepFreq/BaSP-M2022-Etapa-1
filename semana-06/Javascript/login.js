5efe785e6ae21407f9835d9e2115f497e2ec23f6
Alan Acosta <alanegocios77@gmail.com>
2022-04-25 01:32:36 -0300

    BaSP-W06-2022: Start login JS - WIP

---
+++ semana-06/Javascript/login.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+
+    var inputArea = document.getElementsByTagName('input');
+    var confirm = document.getElementById('continue');
+    var email = document.getElementById('inputemail');
+    var password = document.getElementById('inputpass');
+
+    email.addEventListener('focus', (event) => {
+        
+    });
+    email.addEventListener('blur' (event) => {
+
+    })
+    password.addEventListener('focus' (event) => {
+
+    })
+}
