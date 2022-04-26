98c5bdf28a74b1e09f57082b90577e4887fa1bda
Alan Acosta <alanegocios77@gmail.com>
2022-04-26 01:26:42 -0300

    BaSP-W06-2022: Start build function on login - WIP

--- semana-06/Javascript/login.js
+++ semana-06/Javascript/login.js
@@ -1,17 +1,41 @@
+// -Window.onload- es un evento para ejecutar codigo Javascript apenas la pagina haya cargado. De esta manera nos aseguramos que el navegador...
+// ...está ejecutando JS correctamente.
 window.onload = function () {
-
-    var inputArea = document.getElementsByTagName('input');
+    const exprsLogin = {
+        password: /^.{4,12}$/,
+        email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    }
+    // Crear variables usando selectores DOM para acceder a los nodos. Por ejemplo con...
+    //... -getElementsByName- seguido del nombre del elemento.
+    var errorMsj = document.getElementsByClassName('errorMsj');
     var confirm = document.getElementById('continue');
-    var email = document.getElementById('inputemail');
-    var password = document.getElementById('inputpass');
+    var emailInput = document.getElementById('inputemail');
+    var passwordInput = document.getElementById('inputpass');
+    var butonC = document.getElementById('buttonC');
+    console.log(errorMsj);
+    // Agregamos los eventos a las variables creadas. En este caso los eventos 'blur' y 'focus' a traves de...
+    // la propiedad -addEventListener-. Seleccionamos el tipo de evento seguido de una función.
+    emailInput[0].addEventListener('blur', onBlurEmail);
+    emailInput[0].addEventListener('focus', onFocusEmail);
 
-    email.addEventListener('focus', (event) => {
-        
-    });
-    email.addEventListener('blur' (event) => {
+    passwordInput[0].addEventListener('focus', onFocusPass);
+    passwordInput[0].addEventListener('blur', onBlurPass);
 
-    })
-    password.addEventListener('focus' (event) => {
+    button[0].addEventListener('click', clickButton);
 
-    })
+    // Creamos variables para validaciones de error. Estas mismas son usadas luego dentro de las funciones a ejecutar.
+
+    var errorValidationPass;
+    var errorValidationEmail;
+
+    // A continuación creamos las funciones de validaciones.
+    function emailValidation(e) {
+       if (/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(e.target.value)) {
+       errorValidationEmail = e.target.value;
+    } else {
+        errorMsj[0].innerHTML = 'Error en el email';
+        errorMsj[0].style.display = "block";
+        errorValidationEmail = 'Error en el email';
+    }
+    console.log(errorMsj);
 }
