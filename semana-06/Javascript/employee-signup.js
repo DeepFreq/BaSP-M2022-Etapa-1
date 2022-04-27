c5efe202028f869a052305ae4a58d98d00e154af
Alan Acosta <alanegocios77@gmail.com>
2022-04-26 23:07:39 -0300

    BaSP-W06-2022: Signup & Login WIP

--- semana-06/Javascript/employee-signup.js
+++ semana-06/Javascript/employee-signup.js
@@ -18,45 +18,79 @@ window.onload = function () {
         locationRegex: /[A-Za-z0-9? ,_-]/,
     }
 
-    var formValidation = (e) => { // No se ejecuta el codigo 19-30
+    var fields = {
+        name: false,
+        lastName: false,
+        password: false,
+        idNumber: false,
+        phone: false,
+        date: false,
+        adress: false,
+        location: false,
+        postal: false,
+        email: false,
+        repeatpass: false,
+    }
+
+    var formValidation = (e) => { // No se ejecuta el codigo 
        switch (e.target.name) {
            case 'name':
+               validationAll(exprsSignUp.nameRegex, e.target, 'name');
                 
            break;
-           case 'name':
-                
-           break;
+
            case 'last-name':
-                
+                validationAll(exprsSignUp.nameRegex, e.target, 'lastName');
            break;
+
            case 'id-number':
-                
+            validationAll(exprsSignUp.numericRegex, e.target, 'idNumber');
            break;
+
            case 'date':
-                
+            validationAll(exprsSignUp.numericRegex, e.target, 'date');
            break;
+
            case 'phone':
-                
+            validationAll(exprsSignUp.numericRegex, e.target, 'phone');
            break;
+
            case 'adress':
-                
+            validationAll(exprsSignUp.adressRegex, e.target, 'adress');
            break;
+
            case 'location':
-                
+            validationAll(exprsSignUp.locationRegex, e.target, 'location');
            break;
+
            case 'postalCode':
-                
+            validationAll(exprsSignUp.locationRegex, e.target, 'postalCode');
            break;
+
            case 'email':
-                
+            validationAll(exprsSignUp.emailRegex, e.target, 'email');
            break;
+
            case 'password':
-                
+            validationAll(exprsSignUp.passwordRegex, e.target, 'password');
            break;
+
            case 'repeatPass':
-                
+            validationAll(exprsSignUp.passwordRegex, e.target, 'repeatPass');
            break;
        }
+    }
+
+    var validationAll = (exprsLogin, input, fields) => {
+        if(exprsSignUp.test(input.value)) {
+            document.getElementById(`${fields}-form`).classList.remove('form-incorrect-group');
+            document.getElementById(`${fields}-form`).classList.add('form-correct-group');
+            document.getElementById(`${fields}-form`.errorMesage).classList.remove('form-msj-active-exit');
+           }    else {
+               document.getElementById(`${fields}-form`).classList.add('form-incorrect-group');
+               document.getElementById(`${fields}-form`).classList.remove('form-incorrect-group');
+               document.getElementById(`${fields}-form`.errorMesage).classList.add('form-msj-active-exit');
+           }
     }
 
     inputs.forEach((input) => {
@@ -68,8 +102,35 @@ window.onload = function () {
         e.preventDefault();
     });
     
+    var signup = document.getElementById('form');
+    signup.addEventListener('submit', signupsubmit);
 
+    function signupsubmit(e) {
+        e.preventDefault();
+        var form = new FormData(signup);
+        var formFullName = form.get('name');
+        var formdni = form.get('id-number');
+        var formbirthday = form.get('date');
+        var formtel = form.get('phone');
+        var formadress = form.get('adress');
+        var formlocation = form.get('location');
+        var formcp = form.get('postalCode');
+        var formEmail = form.get('email');
+        var formPassword = form.get('password');
+        var formconfirmp = form.get('repeatPass');
+        console.log(form, formFullName)
 
+        if (formFullName.match(fullnameRegex) && formdni.match(numericRegex) && formbirthday && formtel.match(numericRegex) && formadress.match(adressRegex) && formlocation.match(locationRegex) && formcp.match(numericRegex) && formEmail.match(emailRegex) && formPassword.match(passwordRegex) && formconfirmp === formPassword) {
+            error.style.display = 'none';
+            alert('Full Name: ' + formFullName + '\n' + 'DNI: ' + formdni + '\n' + 'Birthday: ' + formbirthday + '\n' +
+                'Tel.: ' + formtel + '\n' + 'Adress: ' + formadress + '\n' + 'Location: ' + formlocation + '\n' +
+                'C.P.: ' + formcp + '\n' + 'E-mail: ' + formEmail + '\n' + 'Password' + formPassword + '\n' +
+                'Confirm Password: ' + formconfirmp);
+        } else {
+            error.style.display = 'block';
+            alert('Wrong Inputs');
+        }
+    }
 
 
 }
