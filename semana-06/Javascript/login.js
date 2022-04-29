48fa10bbe45c7a656ee85156a1455f831a97d80d
Alan Acosta <alanegocios77@gmail.com>
2022-04-29 00:42:15 -0300

    BaSP-W07-2022: Fix login.js | Fix asignation of variables names

--- semana-06/Javascript/login.js
+++ semana-06/Javascript/login.js
@@ -7,21 +7,25 @@ window.onload = function () {
     }
     // Crear variables usando selectores DOM para acceder a los nodos. Por ejemplo con...
     //... -getElementsByName- seguido del nombre del elemento.
-    var errorMsj = document.getElementsByClassName('errorMsj');
+    var errorMsj = document.querySelectorAll('p');
     var confirm = document.getElementById('continue');
-    var emailInput = document.getElementById('inputemail');
-    var passwordInput = document.getElementById('inputpass');
-    var butonC = document.getElementById('buttonC');
+    var emailInput = document.querySelectorAll('input');
+    var passwordInput = document.querySelectorAll('input');
+    var buttonC = document.getElementById('buttonC');
     console.log(errorMsj);
+    console.log(emailInput);
+    console.log(passwordInput);
+    console.log(errorMsj);
+    console.log(buttonC);
     // Agregamos los eventos a las variables creadas. En este caso los eventos 'blur' y 'focus' a traves de...
     // la propiedad -addEventListener-. Seleccionamos el tipo de evento seguido de una función.
     emailInput[0].addEventListener('blur', emailV);
     emailInput[0].addEventListener('focus', cleanV);
 
-    passwordInput[0].addEventListener('focus', passV);
-    passwordInput[0].addEventListener('blur', cleanV);
+    passwordInput[1].addEventListener('focus', passV);
+    passwordInput[1].addEventListener('blur', cleanV);
 
-    button[0].addEventListener('click', clickButton);
+    buttonC[0].addEventListener('click', clickButton);
 
     // Creamos variables para validaciones de error. Estas mismas son usadas luego dentro de las funciones a ejecutar.
 
@@ -33,8 +37,9 @@ window.onload = function () {
        if (/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(e.target.value)) {
        errorValidationEmail = e.target.value;
     } else {
-        errorMsj[0].innerHTML = 'Error en el email';
-        errorMsj[0].style.display = "block";
+        errorMsj[1].innerHTML = 'Error en el email';
+        errorMsj[1].style.display = 'flex';
+        errorMsj[1].style.color = 'red';
         errorValidationEmail = 'Error en el email';
         }
     }
@@ -42,8 +47,9 @@ window.onload = function () {
         if (/[a-z0-9]/.test(e.target.value)) {
           errorValidationPass = e.target.value;
         } else {
-          errorMsj[0].style.display = "block";
-          errorMsj[0].innerHTML = "Error en el password";
+          errorMsj[2].style.display = "block";
+          errorMsj[2].style.color = 'red';
+          errorMsj[2].innerHTML = "Error en el password";
           errorValidationPass = "Error en el password";
         }
     }
@@ -58,26 +64,28 @@ window.onload = function () {
         e.path[0].nextElementSibling.style.display = "none";
     }
 
-    var login = document.getElementById('login');
-    login.addEventListener('submit', emailsubmit);
 
-    function emailsubmit(e) {
-        e.preventDefault();
-        var form = new FormData(login);
-        var formEmail = form.get('inputemail');
-        var formPassword = form.get('inputpassword');
 
-        if (formPassword.match(passwordRegex) && formEmail.match(emailRegex)) {
-            errorMsj.style.display = 'none';
-            inputpass.style.borderColor = '#0F0';
-            inputemail.style.borderColor = '#0F0';
-            alert('Email: ' + formEmail + '\n' + 'Password: ' + formPassword);
-        } else {
-            inputpass.style.borderColor = '#f00';
-            inputemail.style.borderColor = '#f00';
-            errorMsj.style.display = 'block';
-            alert('Email or password incorrect');
-        }
-    }
+    // var login = document.getElementById('form');
+    // login.addEventListener('submit', emailsubmit);
+
+    // function emailsubmit(e) {
+    //     e.preventDefault();
+    //     var form = new FormData(login);
+    //     var formEmail = form.get('inputemail');
+    //     var formPassword = form.get('inputpassword');
+
+    //     if (formPassword.match(passwordRegex) && formEmail.match(emailRegex)) {
+    //         errorMsj.style.display = 'none';
+    //         inputpass.style.borderColor = '#0F0';
+    //         inputemail.style.borderColor = '#0F0';
+    //         alert('Email: ' + formEmail + '\n' + 'Password: ' + formPassword);
+    //     } else {
+    //         inputpass.style.borderColor = '#f00';
+    //         inputemail.style.borderColor = '#f00';
+    //         errorMsj.style.display = 'block';
+    //         alert('Email or password incorrect');
+    //     }
+    // }
 }
 
